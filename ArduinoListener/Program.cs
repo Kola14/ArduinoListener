1f54d583cf05182b46e88febc2e2e295c98b1a21
nik fed <nikfed142@gmail.com>
2019-04-13 18:14:31 +0300

    Add project files.

---
+++ ArduinoListener/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO.Ports;
+
+namespace ArduinoListener
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            //sqllite
+            foreach (string s in SerialPort.GetPortNames())
+            {
+                Console.WriteLine("   {0}", s);
+            }
+
+            var listener = new SerialPortListener("com3", 9600);
+            listener.MessageReceived += ListenerMessageReceived;
+
+            listener.Start();
+
+            Console.ReadLine();
+
+            listener.Stop();
+        }
+
+        private static void ListenerMessageReceived(object sender, MessageReceivedEventArgs e)
+        {
+            Console.WriteLine(e.Message);
+        }
+    }
+}
---
+++ ArduinoListener/SerialPortListener.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO.Ports;
+using System.Threading;
+
+namespace ArduinoListener
+{
+    class SerialPortListener
+    {
+        private readonly string portName;
+        private readonly int baudRate;
+
+        private readonly SerialPort serialPort;
+
+        public event EventHandler<MessageReceivedEventArgs> MessageReceived;
+
+        public SerialPortListener(string portName, int baudRate)
+        {
+            this.portName = portName.ToLower();
+            this.baudRate = baudRate;
+
+            serialPort = new SerialPort(portName, baudRate);
+        }
+
+        public void Start()
+        {
+            serialPort.Open();
+            var thread = new Thread(Listen);
+            thread.Start();
+        }
+
+        private void Listen()
+        {
+            while (serialPort.IsOpen)
+            {
+                string message = serialPort.ReadLine();
+                MessageReceived?.Invoke(this, new MessageReceivedEventArgs(message));
+            }
+        }
+
+        public void Stop()
+        {
+            serialPort.Close();
+        }
+    }
+}
